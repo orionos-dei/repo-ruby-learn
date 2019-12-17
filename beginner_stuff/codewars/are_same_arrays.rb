95f06e07d515cc9a7a324de72b9df20e10899757
orionos-dei <orionos.dei@gmail.com>
2019-12-17 21:37:14 +0100

    Upload Kata answer

---
+++ beginner_stuff/codewars/are_same_arrays.rb
@@ -0,0 +1,10 @@
+def comp(array1, array2)
+  return false if(array1&&array2).nil?
+  array1.map{|i| i*i}.sort == array2.sort
+end
+
+#top answer
+
+def comp(array1, array2)
+  array1.nil? || array2.nil? ? false : array1.sort.map {|v| v ** 2} == array2.sort
+end
