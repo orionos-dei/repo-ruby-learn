4af32051f4837d8c2dc95f553fc36b2d929389e2
orionos-dei <orionos.dei@gmail.com>
2019-12-28 19:38:15 +0100

    Iteration 1: Parsing with CSV complete.

--- beginner_stuff/event_manager/lib/event_manager.rb
+++ beginner_stuff/event_manager/lib/event_manager.rb
@@ -1,1 +1,10 @@
+require 'csv'
 puts "EventManager Initialized!"
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
+end
+
