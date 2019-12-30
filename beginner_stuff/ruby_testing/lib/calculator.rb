8de34a3184b31f9b84d923aa5a514cee1203b2aa
orionos-dei <orionos.dei@gmail.com>
2019-12-30 22:44:49 +0100

    Excercise complete

--- beginner_stuff/ruby_testing/lib/calculator.rb
+++ beginner_stuff/ruby_testing/lib/calculator.rb
@@ -1,5 +1,11 @@
 class Calculator
   def add(*a)
+    a.map!(&:to_i) unless nil
     a.inject(:+)
   end
+  def multiply(*a)
+    a.map!(&:to_i) unless nil
+    a.inject(:*)
+  end
+
 end
--- beginner_stuff/ruby_testing/spec/calculator_spec.rb
+++ beginner_stuff/ruby_testing/spec/calculator_spec.rb
@@ -11,5 +11,39 @@ RSpec.describe Calculator do
       calculator = Calculator.new
       expect(calculator.add(1,2,3)).to eql(6)
     end
+
+    it "returns nil if the def has 0 integers" do
+      calculator = Calculator.new
+      expect(calculator.add()).to eql(nil)
+    end
+
+    it "converts strings to integers" do
+      calculator = Calculator.new
+      expect(calculator.add("one","two")).to eql(0)
+      expect(calculator.add("2",3)).to eql(5)
+    end
+  end
+  describe "#multiply" do
+    it "returns the * of two integers" do
+      calculator = Calculator.new
+      expect(calculator.multiply(5,2)).to eql(10)
+    end
+
+    it "returns the * of more than two integers" do
+      calculator = Calculator.new
+      expect(calculator.multiply(1,2,3)).to eql(6)
+    end
+
+    it "returns nil if the def has 0 integers" do
+      calculator = Calculator.new
+      expect(calculator.multiply()).to eql(nil)
+    end
+
+    it "converts strings to integers" do
+      calculator = Calculator.new
+      expect(calculator.multiply("one","two")).to eql(0)
+      expect(calculator.multiply("2",3)).to eql(6)
+    end
+
   end
 end
